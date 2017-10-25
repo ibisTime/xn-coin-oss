137e3e817f1f8457a8d133b3a2137ffd9f7c4250
hss1994 <1070798770@qq.com>
2017-10-25 16:19:23 +0800

    v1.0.0

---
+++ static/js/config.js
@@ -0,0 +1,21 @@
+var OSS = {};
+// mainUrl
+OSS.mainUrl = '/mainUrl';
+// system
+OSS.system = 'CD-COIN000017';
+// companyCode
+OSS.company = 'CD-COIN000017';
+// system name
+OSS.systemName = '以太坊管理平台';
+// company name
+OSS.companyName = '杭州橙袋科技有限公司';
+// menuCode
+OSS.menuRoot = 'COINSM201700000000000000';
+// picUrl
+OSS.picBaseUrl = sessionStorage.getItem('qiniuUrl');
+// picShow
+OSS.picShow = '?imageMogr2/auto-orient/interlace/1';
+//系统登录有效时间
+OSS.userValidTime = 30;
+//系统userid
+OSS.SYS_USER = 'sysAdmin';
